f0c6f9d8f9bb849ab636475ed70f1ed1fe708ba7
Guylian Gilsing (School Laptop) <78902@roc-teraa.nl>
2021-11-11 16:33:46 +0100

    Working socket server

--- src/main/java/com/horecarobot/robotcommunication/RobotCommunicationApplication.java
+++ src/main/java/com/horecarobot/robotcommunication/RobotCommunicationApplication.java
@@ -1,4 +1,4 @@
-package com.horecarobot.communication.robotcommunication;
+package com.horecarobot.robotcommunication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
